bd9da70a72e84d96e13d5a4d362cb8b0a8236d98
sonh <sonh@runsystem.net>
2020-01-11 17:14:03 +0900

    init project

---
+++ app/src/main/java/run/nguyen/hs/gmo/realmtableandroid/TableMainActivity.kt
@@ -0,0 +1,12 @@
+package run.nguyen.hs.gmo.realmtableandroid
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class TableMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_table_main)
+    }
+}
